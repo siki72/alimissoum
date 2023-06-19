3b0d6c3ef04c51e2357758e8011d9c6cab4d7c1b
siki72 <missoum.ali.hichem@gmail.com>
2023-06-19 20:13:55 +0200

    navigation added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-
+import React from "react";
+import styles from "./page.module.css";
 const page = () => {
-    return (
-        <div>
-            home
+  return (
+    <div className={styles.container}>
+      <section className={styles.hero}>
+        <div className={styles.welcom}>
+          <p>Bienvenue Dans Mon Portfolio</p>
         </div>
-    );
+        <div className={styles.title}>
+          <h1>Hello, je m&apos;appelle Ali</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            placeat accusantium dignissimos deserunt.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default page;
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,62 @@
+"use client"
+import Link from 'next/link.js';
 import React from 'react';
+import styles from "./navbar.module.css"
+import Image from 'next/image.js';
 
 const NavBar = () => {
+    const links = [
+        {
+            id: 1,
+            title: "Home",
+            url: "/",
+        },
+        {
+            id: 2 ,
+            title: "Portfolio",
+            url: "/portfolio",
+        },
+        {
+            id: 3,
+            title: "Blog",
+            url: "/blog",
+        },
+        {
+            id: 4,
+            title: "About",
+            url: "/about",
+        },
+        {
+            id: 5,
+            title: "Contact",
+            url: "/contact",
+        },
+        {
+            id: 6,
+            title: "Dashboard",
+            url: "/dashboard",
+        },
+        
+    ]
     return (
-        <div>
-            navbar
-        </div>
+        <nav className={styles.container}>
+            <Link  href="/" className={styles.logo}>
+                <Image className={styles.img} src="/img/profile.png" width={80}height={80} alt='profile image' /><span className={styles.name}>Ali Missoum</span>
+            </Link>
+            <ul className={styles.ul}>
+                {links.map(link => (
+                    <li key={link.id} className={styles.li}>
+                        <Link href={link.url}>{link.title}</Link>
+                    </li>
+                ))}
+                <button
+          className={styles.logout}
+          onClick={() => console.log("logged out")}
+        >
+          Logout
+        </button>
+            </ul>
+        </nav>
     );
 };
 
