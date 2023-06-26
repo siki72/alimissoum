b5a244f4a456769f7dc0ec1bcfe7617dfda73b87
siki72 <missoum.ali.hichem@gmail.com>
2023-06-26 10:57:24 +0200

    color design added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,9 +12,10 @@ const page = () => {
     <div className={styles.container}>
       <section className={styles.hero}>
         <div className={styles.welcom}>
-          <h1>Bonjour,<br /> je suis ali missoum</h1>
-          <p className={styles}>Développeur full-stack freelance Node / Express / SQL / JavaScript / React / Nextjs. <br />
-            </p>
+          <h2>Bonjour, je suis Ali Missoum</h2>
+          <h1 className={styles}>Développeur full-stack freelance Node / Express / SQL / JavaScript / React / Nextjs. <br />
+            </h1>
+            <p> <span className={styles.colorEffect}>Ensemble</span>, créons une interface web intelligente, offrant une expérience <span className={styles.colorEffect}>utilisateur</span> optimale</p>
             <button className={styles.btn}>mes projets</button>
         </div>
         <div className={styles.imgcontainer}>
@@ -22,7 +23,7 @@ const page = () => {
             <Image className={styles.img} src="/img/profile.png" width={80} height={80} alt="pic" />
           </div>
           <p>  
-              Je suis un développeur front-end freelance, basé sur Le Mans.
+              Je suis un développeur full-stack freelance orienté front-end , basé sur Le Mans.
               Vous trouverez ici plus d&apos;informations sur moi, ce que je fais, et mes compétences actuelles, principalement en termes de programmation et de technologie.
               Je suis ouvert aux opportunités d&apos;emploi où je peux collaborer, apprendre et acroitrre mes compétances. Si vous avez un projets à réaliser, n&apos;hésitez pas à me contacter. </p> 
         </div>
