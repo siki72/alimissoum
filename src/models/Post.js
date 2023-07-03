4b61aa9e7ac68179234566b5bf20dbb467232574
siki72 <missoum.ali.hichem@gmail.com>
2023-07-03 12:33:17 +0200

    added models

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,28 +1,34 @@
 import mongoose from "mongoose";
+let postModel;
 
-const postSchema = new mongoose.Schema({
-    title:{
-      type: String,
-      required: true
+try {
+  postModel = mongoose.model("Post");
+} catch (err) {
+  const postSchema = new mongoose.Schema(
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      desc: {
+        type: String,
+        required: true,
+      },
+      img: {
+        type: String,
+        required: true,
+      },
+      content: {
+        type: String,
+        required: true,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
     },
-    desc:{
-      type: String,
-      required: true
-    },
-    img:{
-      type: String,
-      required: true
-    },
-    content:{
-      type: String,
-      required: true
-    },
-    username:{
-      type: String,
-      required: true
-    },
-      
-}, {timestamps: true});
- 
-const postModel =  mongoose.model("Post", postSchema)
-export default postModel
+    { timestamps: true }
+  );
+  postModel = mongoose.model("Post", postSchema);
+}
+export default postModel;
